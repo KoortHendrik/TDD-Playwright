aae70b82d944eb8415fa18345d5567c39dea3584
Hendrik Koort <hekoor@taltech.ee>
2026-06-15 14:42:59 +0300

    Add generated test test-endpoint-get.smoke.js

--- tests/api/smoke/test-endpoint-get.smoke.js
+++ tests/api/smoke/test-endpoint-get.smoke.js
@@ -1,6 +1,9 @@
 const { test, expect } = require('@playwright/test');
-test('test endpoint get', async ({ page }) => {
-  const url = `${process.env.BASE_URL || 'http://localhost:3000'}/test-endpoint`;
-  const response = await page.goto(url);
-  expect(response.ok()).toBeTruthy();
+
+test('[api] [smoke] test-endpoint loads without API errors', async ({ page }) => {
+    const url = `${process.env.BASE_URL || 'http://localhost:3000'}/test-endpoint`;
+    const response = await page.goto(url);
+    expect(response.ok()).toBeTruthy();
+    const responseBody = await response.json();
+    expect(responseBody).toHaveProperty('response');
 });
